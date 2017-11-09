cf9620970dbd09f27e391c0582f29e014fc209d7
Roberto Segura - phproberto <roberto@phproberto.com>
2017-11-09 10:06:05 +0100

    [tests] JUser unit tests

---
+++ tests/Unit/Extension/JUserTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @package     Phproberto.Joomla-Twig
+ * @subpackage  Tests.Unit
+ *
+ * @copyright  Copyright (C) 2017 Roberto Segura López, Inc. All rights reserved.
+ * @license    See COPYING.txt
+ */
+
+namespace Phproberto\Joomla\Twig\Tests\Unit\Extension;
+
+use Joomla\CMS\Factory;
+use Phproberto\Joomla\Twig\Extension\JUser;
+
+/**
+ * JUser extension test.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class JUserTest extends \TestCase
+{
+	use Traits\HasFunctions;
+
+	private $extension;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @return  void
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->extension = new JUser;
+	}
+
+	/**
+	 * Test getFunctions returns correct data.
+	 *
+	 * @return  void
+	 */
+	public function testGetFunctions()
+	{
+		$this->genericFunctionsTest();
+	}
+
+	/**
+	 * getName returns correct name.
+	 *
+	 * @return  void
+	 */
+	public function testGetNameReturnsCorrectName()
+	{
+		$this->assertEquals('juser', $this->extension->getName());
+	}
+
+	/**
+	 * Functions expected to be added by the extension.
+	 *
+	 * @return  array
+	 */
+	protected function expectedFunctions()
+	{
+		return [
+			'juser'        => [
+				'class'  => Factory::class,
+				'method' => 'getUser'
+			]
+		];
+	}
+}
